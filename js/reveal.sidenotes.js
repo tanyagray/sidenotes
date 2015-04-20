4ed73e323b313d8e198bbe5a091d4dd68fc1ba7a
Tanya Gray <Tanya@Wonkbook.local>
2015-04-20 17:49:46 +1200

    Initial Commit (untested)
    Signed-off-by: Tanya Gray <Tanya@Wonkbook.local>

---
+++ js/reveal.sidenotes.js
@@ -0,0 +1,16 @@
+/*
+Author: Tanya Gray
+
+Sidenotes is a plugin for reveal.js 
+
+Version: 0.1
+ 
+License: MIT license (see LICENSE.md)
+*
+*/
+
+(function(){
+
+	
+
+})();
---
+++ reveal.sidenotes.js
@@ -0,0 +1,55 @@
+/*
+Author: Tanya Gray
+
+Sidenotes is a plugin for reveal.js which displays
+speaker notes in a docked sidebar, visible to the
+viewer.
+
+Version: 0.1
+ 
+License: MIT license (see LICENSE.md)
+*
+*/
+
+(function(){
+
+	/*
+	Show notes on slideshow load
+	*/
+	Reveal.addEventListener( 'ready', function( event ) {
+		copyNotes(event.currentSlide);
+		checkNotesState();
+	} );
+
+	/*
+	Update notes on slide changed
+	*/
+	Reveal.addEventListener( 'slidechanged', function( event ) {
+	    copyNotes(event.currentSlide);
+	} );
+
+	/*
+	Copy notes to visible notes section
+	*/
+	function copyNotes(slide) {
+		notes = $(slide).find('aside.notes').html();
+	    $('.inline-notes .content').html( marked(notes) );
+	}
+
+	/*
+	Toggle sidenotes visibility
+	*/
+	function showNotes() {
+		$("body").toggleClass("with-notes");
+	}
+
+	/*
+	Listen for enter key to toggle notes
+	*/
+	$(document).keypress(function(e) {
+	  	if(e.which == 13) {
+	  		showNotes();
+	  	}
+	});
+
+})();
