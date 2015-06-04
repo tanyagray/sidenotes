d3b6596c01689ab7f3d7cc8c467dc969dc73cd15
Tanya Gray <Tanya@Wonkbook-2.local>
2015-06-04 19:07:20 +1200

    Super simplification of sidenotes structure plus bug fix.
    Signed-off-by: Tanya Gray <Tanya@Wonkbook-2.local>

--- sidenotes.js
+++ sidenotes.js
@@ -8,83 +8,48 @@ viewer.
 Version: 0.1
  
 License: MIT license (see LICENSE.md)
+*
 */
 
 window.Sidenotes || (window.Sidenotes = function (Reveal) {
 
-	var defaultNote = "No additional notes :)";
+	/*
+	Show notes on slideshow load
+	*/
+	Reveal.addEventListener( 'ready', function( event ) {
+		copyNotes(event.currentSlide);
+		checkNotesState();
+	} );
 
 	/*
-	 * Load the first set of notes and watch for changes
-	 */
-	function init() {
-		createNotes();
+	Update notes on slide changed
+	*/
+	Reveal.addEventListener( 'slidechanged', function( event ) {
+	    copyNotes(event.currentSlide);
+	} );
 
-		Reveal.addEventListener('ready', updateSidenotes);
-		Reveal.addEventListener('slidechanged', updateSidenotes);
-
-		document.onkeypress = handleKeypress; 
-
-		updateSidenotes();
+	/*
+	Copy notes to visible notes section
+	*/
+	function copyNotes(slide) {
+		notes = $(slide).find('aside.notes').html();
+	    $('.inline-notes .content').html( marked(notes) );
 	}
 
 	/*
-	 * Create the divs to contain the notes.
-	 */
-	function createNotes() {
-		var sidenotes = document.createElement('div');
-		sidenotes.classList.add('inline-notes');
-
-  		document.body.appendChild(sidenotes); 
-
+	Toggle sidenotes visibility
+	*/
+	function showNotes() {
+		$("body").toggleClass("with-notes");
 	}
 
 	/*
-	 * Copy notes to visible notes section
-	 */
-	function updateSidenotes() {
-		var currentSlide = Reveal.getCurrentSlide();
-		var speakerNotes = currentSlide.getElementsByClassName('notes')[0];
-		var sidenotes = document.getElementsByClassName('inline-notes')[0];
-
-		// notes div is undefined if 
-		// no speaker notes are available
-		if(speakerNotes) {
-			sidenotes.innerHTML = speakerNotes.innerHTML;
-			sidenotes.classList.remove('no-notes');
-		} else {
-			sidenotes.innerHTML = defaultNote;
-			sidenotes.classList.add('no-notes');
-		}
-	}
-
-	/*
-	 * Handle all keypress events for the page
-	 */
-	function handleKeypress(keyEvent){
-	  	// 13: Enter Key
-	  	if(keyEvent.which == 13) {
+	Listen for enter key to toggle notes
+	*/
+	$(document).keypress(function(e) {
+	  	if(e.which == 13) {
 	  		showNotes();
 	  	}
-	}
-
-	/*
-	 * Toggle sidenotes visibility
-	 */
-	function showNotes() {
-		document.body.classList.toggle('with-notes');
-
-		// blegh.
-		// workaround for layout not triggering
-		// on sidenotes show/hide.
-		// sidenotes cover slide on some screen 
-		// sizes without this.
-		setTimeout(function() {
-		    window.dispatchEvent( new Event('resize') );
-		    console.log('resize');
-		}, 700);
-	}
-
-	init();
+	});
 
 }(Reveal));
